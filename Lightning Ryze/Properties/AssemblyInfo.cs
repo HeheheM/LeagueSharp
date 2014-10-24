fc6f2fbf70ec4699060046cad23d398728e7df67
HeheheM <kefirowaty123@gmail.com>
2014-10-25 00:32:50 +0200

    Update AssemblyInfo.cs

--- Lightning Ryze/Properties/AssemblyInfo.cs
+++ Lightning Ryze/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lightning Ryze")]
+[assembly: AssemblyTitle("Lightning Ryze Edit HeheheM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
